4d1525fe08220bef538722d6f9c76a13075b7c8b
Decolour <decolourrr@gmail.com>
2024-07-02 09:49:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+class Task():
+    def __init__(self):
+        self.tasks = []
 
-print('ftui')
+    def add_task(self, deadline, description):
+        self.tasks.append({'deadline': deadline, 'description': description, 'status': 'не выполнено'})
 
-print('ghj')
+    def complete_tasks(self, description):
+        for task in self.tasks:
+            if task['descripton'] == description:
+                task['status'] = 'выполнено'
+                print(f'Задача "{description}" успешно выполнена!')
+            else:
+                print(f'Задача "{description}" не найдена.')
+
+    def show_tasks(self):
+        print(f'Список задач:')
+        for task in self.tasks:
+            if task['status'] == 'не выполнено':
+                print(f"{task['description']} - {task['deadline']}")
 
 
-a = 4
-b = 8
-c = 9
+t = Task()
+t.add_task('сегодня', 'помыть картошку')
+t.add_task('завтра', 'почистить картошку')
+
+t.show_tasks()
